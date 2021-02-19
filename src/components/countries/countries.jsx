2a6e9f3652668ccbe8cc6357981d342734523d48
marsoon <asroq98@gmail.com>
2021-02-19 18:46:47 +0900

    add object data

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,26 +1,631 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// 프로필에서 사용 할 데이터
 const countries = {
-	ESTJ: {
-		name: 'marsoon',
-		description: 'it works !!!!!! Fuck you I won_t give up',
-	},
-	velopert: {
-		name: '김민준',
-		description:
-			'Frontend Engineer @ Laftel Inc. 재밌는 것만 골라서 하는 개발자',
-	},
-	gildong: {
-		name: '홍길동',
-		description: '전래동화의 주인공',
-	},
-	spain: {
-		name: 'barcelona',
-		description: '태양의 도시 !',
+	INTJ: {
+		subhead: '아는 만큼 보인다! 여행모범생',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
 		img:
 			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ENTP',
+				des: '남는 건 결국 음식이야 식도락 여행!',
+			},
+		],
+		counter: [
+			{
+				subhead: 'ESFP',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	INTP: {
+		subhead: '혼자라서 즐거워요~ 자유가 최고야',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ENTJ',
+				des: '여행지에서 국밥처럼 든든한',
+			},
+		],
+		counter: [
+			{
+				subhead: 'ESFJ ',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	INFJ: {
+		subhead: '여행만큼 좋은 힐링이 있을까?',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ENFP',
+				des: '물 흐르듯 흘러가는대로 여행하는 자유로운 영혼',
+			},
+		],
+		counter: [
+			{
+				subhead: '으윽 정반대의 어쩌고 저쪼고',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	INFP: {
+		subhead: '영화나 드라마에서 나오는 장소의 주인공 ',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ENFJ',
+				des: '작은 세세한 것까지 다 아름다운 감수성이 넘치는 스타일',
+			},
+		],
+		counter: [
+			{
+				subhead: '으윽 정반대의 어쩌고 저쪼고',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	ISTJ: {
+		subhead: '여행계획은 출발 세 달 전부터 세우는 거야',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ESTP',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'ENFP',
+				des: '물 흐르듯 흘러가는대로 여행하는 자유로운 영혼',
+			},
+		],
+	},
+	ISFJ: {
+		subhead: '여유롭지만 가성비 좋은 여행이 좋아',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: '으윽 정반대의 어쩌고 저쪼고',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'ENTP',
+				des: '남는 건 결국 음식이야 식도락 여행!',
+			},
+		],
+	},
+	ISTP: {
+		subhead: '혼자가 좋지만 그렇다고 심심한 건 싫어',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ESTJ',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'ENFJ',
+				des: '작은 세세한 것까지 다 아름다운 감수성이 넘치는 스타일',
+			},
+		],
+	},
+	ISFP: {
+		subhead: '여행계의 메딕 힐링여행 러버!',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ESFJ',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'ENTJ',
+				des: '여행지에서 국밥처럼 든든한',
+			},
+		],
+	},
+	ESTP: {
+		subhead: '우리는 모두 친구! 모든 지구인들은 내 친구',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '인싸형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ISTJ',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'INFJ',
+				des: '여행만큼 좋은 힐링이 있을까?',
+			},
+		],
+	},
+	ESFP: {
+		subhead: '여행계의 얼리어답터',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ISFJ',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'INTJ',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+
+	ENTJ: {
+		subhead: '여행지에서 국밥처럼 든든한',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'INTP',
+				des: '혼자라서 즐거워요~ 자유가 최고야',
+			},
+		],
+		counter: [
+			{
+				subhead: '으윽 정반대의 어쩌고 저쪼고',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	ENTP: {
+		subhead: '남는 건 결국 음식이야 식도락 여행!',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: '으윽 정반대의 어쩌고 저쪼고',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: '으윽 정반대의 어쩌고 저쪼고',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	ENFJ: {
+		subhead: '작은 세세한 것까지 다 아름다운 감수성이 넘치는 스타일',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'INFP',
+				des: '영화나 드라마에서 보는 로맨틱한 여행지!',
+			},
+		],
+		counter: [
+			{
+				subhead: '으윽 정반대의 어쩌고 저쪼고',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	ENFP: {
+		subhead: '물 흐르듯 흘러가는대로 여행하는 자유로운 영혼',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'INFJ',
+				des: '여행만큼 좋은 힐링이 있을까?',
+			},
+		],
+		counter: [
+			{
+				subhead: 'ISTJ',
+				des: '1분도 허투루 버리지 않는 철저한 계획의 끝판왕',
+			},
+		],
+	},
+	ESTJ: {
+		subhead: '타고난 리더! 여행 단톡방의 독재자',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ISTP',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'INFP',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+	},
+	ESFJ: {
+		subhead: '사랑하는 사람들과 사랑하는 장소를 함께!',
+		subject: '조용한 어쩌고 저쩌고 열정의 스페인!',
+		nickname: '과학자형',
+		img:
+			'https://i2.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/HandLuggageOnly-12.jpg?w=1600&ssl=1',
+		description: [
+			{
+				des: '1111111111',
+			},
+			{
+				des: '222',
+			},
+			{
+				des: '33',
+			},
+			{
+				des: '44',
+			},
+			{
+				des: '5555',
+			},
+			{
+				des: '66',
+			},
+		],
+		duo: [
+			{
+				subhead: 'ISFP',
+				des: '이런 친구랑 함께 하다간 정말 큰일나는 여행일거예요 !',
+			},
+		],
+		counter: [
+			{
+				subhead: 'INTP',
+				des: '혼자라서 즐거워요~ 자유가 최고야',
+			},
+		],
 	},
 }
 
@@ -33,11 +638,44 @@ const Profile = ({ match }) => {
 	}
 	return (
 		<>
-			<h3>
-				{countryName}({nation.name})
-			</h3>
-			<p>{nation.description}</p>
+			<h>당신에게 제일 어울리는 여행지는</h>
+			<h3>{nation.subject}</h3>
+
 			<img src={nation.img} alt="img" width="500px" height="500px" />
+			<div>
+				{nation.nickname}
+				{countryName}의 여행스타일은 ?
+			</div>
+			<div>{nation.subhead}</div>
+			<ul>
+				{nation.description.map(item => {
+					return <li>{nation.description[item]}</li>
+				})}
+			</ul>
+			<div>
+				<div>
+					<img
+						src="https://i.pinimg.com/564x/f3/59/bb/f359bbfa27bf5d6e975621037aa78cc8.jpg"
+						alt=""
+						width="100px"
+						height="100px"
+					/>
+					<b>함께하면 좋아요 !</b>
+					<p>{nation.duo[0].des}</p>
+					<p>찰떡궁합 그 자체{nation.duo[0].subhead}</p>
+				</div>
+				<div>
+					<img
+						src="https://i.pinimg.com/564x/5a/85/22/5a8522a5d349d6adcbdb47d135d1d0f1.jpg"
+						alt=""
+						width="100px"
+						height="100px"
+					/>
+					<b>가능하면 피하는게 좋겠어요 !</b>
+					<p>{nation.counter[0].des}</p>
+					<p>도망가세요 {nation.counter[0].subhead}</p>
+				</div>
+			</div>
 			<Link to="/">
 				<button>다시하기</button>
 			</Link>
--- src/components/options/option.jsx
+++ src/components/options/option.jsx
@@ -5,197 +5,195 @@ const Options = () => {
 	const questions = [
 		{
 			option: 'EI',
-			question: '이번 여행은 어디로 떠나볼까?',
+			question:
+				'여행지에 도착한 첫날 밤. 게스트하우스에서 처음 본 다른 여행자 나의 반응은?',
 			answers: [
 				{
 					type: 'E',
 					score: 2,
-					content: '수 많은 리뷰로 검증된 장소로 가자!',
+					content: '인사를 하며 여행에 관한 이야기를 나눈다 ',
 				},
 				{
 					type: 'I',
 					score: 5,
-					content: '아무도 안 가본 새로운 장소로 떠나자!',
+					content: '짧은 인사만 하고 내일을 위해 빠른 잠을 청한다',
 				},
 			],
 		},
 		{
 			option: 'EI',
-			question: '	우리 여행지 가서 뭐할까?',
+			question:
+				'버스를 한 정거장 빨리 내린 당신. 나처럼 실수하고 같이 내린 또래의 이성',
 			answers: [
 				{
 					type: 'E',
 					score: 2,
-					content: '자전거, 카약 등 다양한 액티비티를 하자!',
+					content: '심심하니 말걸며 함께 걸어간다.',
 				},
 				{
 					type: 'I',
 					score: 5,
-					content: '편안한 장소에서 느긋하게 힐링하자!',
+					content: '버스를 다시 기다리며 정류장에 서있는다.',
 				},
 			],
 		},
 		{
 			option: 'EI',
-			question: '친구와 함께하는 여행 VS 혼자 떠나는 여행',
+			question: '여행 마지막 날 도시에 열린 큰 축제',
 			answers: [
 				{
 					type: 'E',
 					score: 2,
-					content: '역시 친구랑 함께하는 우정여행이 최고야 ! ',
+					content: '내 여행의 마지막을 축제로 아름답게 마무리 할래',
 				},
 				{
 					type: 'I',
 					score: 5,
-					content: '여행은 혼자 배낭 메고 떠나는게 낭만 아닐까 ?',
+					content: '여행동안 좋았던 곳들을 한 번 더 다녀와야겠어',
 				},
 			],
 		},
 		{
 			option: 'SN',
-			question: '화려한 건축물을 보며 드는 생각은 ?',
+			question: '같은 가격에 오래 걸리는 야간열차 VS 빠른 비행기',
 			answers: [
 				{
 					type: 'S',
 					score: 2,
-					content: '어떻게 저걸 지었을까 ? 라며 고민한다',
+					content: '빠른게 장땡이지 바로 비행기',
 				},
 				{
 					type: 'N',
 					score: 5,
-					content: '와 지린다.,.. 하고 감탄한다',
+					content: '뭐야뭐야 완전 낭만적이잖아 바로 야간열차',
 				},
 			],
 		},
 		{
 			which: 'SN',
-			question: '친구가 쓸데없는 기념품을 살 때',
+			question: '기념품 가게에서 발견한 너무 마음에 드는 기념품',
 			answers: [
 				{
 					type: 'S',
 					score: 2,
-					content: '그래 니가 행복하다면 됐어...',
+					content: '결국엔 이쁜 쓰레기. 돈을 아끼자',
 				},
 				{
 					type: 'N',
 					score: 5,
-					content: '그거 결국 쓰레기 된다 ',
+					content: '이런 걸 어디서 또 팔겠어 바로 구입',
 				},
 			],
 		},
 		{
 			which: 'SN',
-			question: '나는 여행지를 선택할 때 주로',
+			question: '아 고민돼 도대체 어딜 가지 ?',
 			answers: [
 				{
 					type: 'S',
 					score: 2,
-					content: '사람이 많은 도시로',
+					content: '사람들이 많이 가는 유럽 ',
 				},
 				{
 					type: 'N',
 					score: 5,
-					content: '나무가 많은 자연으로',
+					content: '나는 남들이 다 가는 곳은 싫어 남미 ',
 				},
 			],
 		},
 		{
 			option: 'TF',
-			question: '숙소를 구할 때',
+			question: '갑자기 나에게 교통비가 모자르다며 돈을 빌려달라는 외국인',
 			answers: [
 				{
 					type: 'T',
 					score: 2,
-					content: '저녁에 바비큐 파티를 여는 곳',
+					content: '정중하게 거절한다',
 				},
 				{
 					type: 'F',
 					score: 5,
-					content: '조용하고 아늑한 곳',
+					content: '안타까우니 남는 동전을 준다',
 				},
 			],
 		},
 		{
 			option: 'TF',
-			question: '여행지에 대한 감상을',
+			question: '다음 도시로 떠나는 마지막 버스를 놓친 나',
 			answers: [
 				{
 					type: 'T',
 					score: 2,
-					content: '말로 내뱉어야 직성이 풀린다',
+					content: '바로 다음날 차를 알아본다.',
 				},
 				{
 					type: 'F',
 					score: 5,
-					content: '내 마음 속에 저_장, 마음에 담고 느낀다',
+					content: '에라 모르겠다 일단 밥부터 먹자',
 				},
 			],
 		},
 		{
 			option: 'TF',
-			question:
-				'고민을 얘기하는 친구, 듣다 보니 친구의 잘못인 것 같다. 그럴 때 나는?',
+			question: '여행지에서 만난 마음에 드는 이성',
 			answers: [
 				{
 					type: 'T',
 					score: 2,
-					content: '친구의 잘못된 점을 말해준다.',
+					content: '스쳐가는 인연이니 남은 일정을 위해 헤어진다',
 				},
 				{
 					type: 'F',
 					score: 5,
-					content: '직접적으로 말하면 친구가 그래 할까봐 돌려 말한다.',
+					content: '일정은 이미 안중에도 없다. 이 사람과 같이 여행하고 싶어',
 				},
 			],
 		},
 		{
 			option: 'JP',
-			question: '준비물을 준비할 때 나는?',
+			question: '혼자 여행 VS 친구와 함께 여행',
 			answers: [
 				{
 					type: 'J',
 					score: 2,
-					content: '하루 전 날 미리 준비한다.',
+					content: '혼자는 무서워 ㅜㅜ 친구랑 갈거야',
 				},
 				{
 					type: 'P',
 					score: 5,
-					content: '‘내일 챙겨야지’ 하다가 까먹고 놓고 오는 일이 많다.',
+					content: '역시 여행은 혼자지 ...',
 				},
 			],
 		},
 		{
 			option: 'JP',
-			question:
-				'끝나고 집에 가서 공부하려 했는데, 친구들이 놀자고 붙잡는다. 나는?',
+			question: '나의 여행스타일은?',
 			answers: [
 				{
 					type: 'J',
 					score: 2,
-					content: '계획에 없던 일인데…매우 당황스럽다.',
+					content: '잠자기 전까지 모든 것을 스케줄링 한다',
 				},
 				{
 					type: 'P',
 					score: 5,
-					content: '오케이! 역시 계획대로 안 되는 것이 인생! 놀자!!!!',
+					content: '구글맵과 함께라면 내가 가는 그곳이 어디든 그곳이 여행지',
 				},
 			],
 		},
 		{
 			option: 'JP',
-			question: '내가 가고싶은 여행지 스타일은?',
+			question: '여행지에 맛집 찾는 방법',
 			answers: [
 				{
 					type: 'J',
 					score: 2,
-					content:
-						'내가 계획한대로! 계획적으로 일의 순서대로 진행하는 것을 좋아해!',
+					content: '맛집 리스트를 쭉 뽑아와서 골라 간다!',
 				},
 				{
 					type: 'P',
 					score: 5,
-					content:
-						'그때그때 생각나는 대로 일 처리! 융통성 있게 하는 것을 좋아해!',
+					content: '맛있어 보이면 곧장 들어가서 먹는다',
 				},
 			],
 		},
